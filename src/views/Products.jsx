14272315dcf0e7e9174258869d302348d1028d17
Ergashev-Xurshid <xurshidgithub@gmail.com>
2025-05-04 23:51:29 +0500

    add product

--- src/views/Products.jsx
+++ src/views/Products.jsx
@@ -1,7 +1,40 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { time } from '../assets'
+import { toast } from 'react-toastify'
+import { getToken } from '../utils/auth'
 
 function Products() {
+
+
+  const [data, setData] = useState([])
+
+  const getProducts = async () => {
+    const res = await fetch("https://back.ifly.com.uz/api/product?page=1&limit=10&min_sell=1")
+    const item = await res.json()
+    setData(item?.data?.products);
+    console.log(item?.data?.products);
+  }
+
+  useEffect(() => {
+    getProducts()
+  }, [])
+
+
+
+  const deleteProduccts = async (id) => {
+    const res = await fetch(`https://back.ifly.com.uz/api/product/${id}`,{
+        method:"Delete",
+        headers:{"Authorization": `Bearer ${getToken()}`}
+    })
+    const item = await res.json();
+    if (item?.success) {
+      toast.success(item?.data?.message)
+      getProducts()
+    } else {
+      toast.error(item?.message?.message)
+    }
+  }
+
   return (
     <div className='shadow-md p-6 bg-white rounded-lg'>
       <div className='flex  justify-between'>
@@ -26,24 +59,29 @@ function Products() {
             </tr>
           </thead>
           <tbody>
-            <tr className='text-center hover:bg-gray-100'>
-              <td className='border border-gray-300 p-2'>1</td>
-              <td className='border border-gray-300 p-2 cursor-pointer w-[150px] h-[100px]'>
-                <img src={time} className='w-full h-full rounded-sm' alt="img" />
-              </td>
-              <td className='border border-gray-300 p-2'>test</td>
-              <td className='border border-gray-300 p-2'>test</td>
-              <td className='border border-gray-300 p-2'>17</td>
-              <td className='border border-gray-300 p-2'>preum</td>
-              <td className='border border-gray-300 p-2'>red</td>
-              <td className='border border-gray-300 p-2'>1700</td>
-              <td className='border border-gray-300 p-2'>80%</td>
-              <td className='border border-gray-300 p-2'>100%</td>
-              <td className='border border-gray-300 p-2'>
-                <button className='px-4 py-2 mr-2 bg-yellow-500 text-white rounded-lg hover:bg-yellow-600 transition cursor-pointer'>Edit</button>
-                <button className='px-4 py-2 bg-red-500 text-white rounded-lg hover:bg-red-600 transition cursor-pointer'>Delete</button>
-              </td>
-            </tr>
+            {data.map((item, i) => (
+              <tr key={item.id} className='text-center hover:bg-gray-100'>
+                <td className='border border-gray-300 p-2'>{i + 1}</td>
+                <td className='border border-gray-300 p-2 cursor-pointer w-[150px] h-[100px]'>
+                  <img src={`https://back.ifly.com.uz/${item?.images?.[0]}`} className='w-full h-full rounded-sm' alt="img" />
+                </td>
+                <td className='border border-gray-300 p-2'>{item?.title_en}</td>
+                <td className='border border-gray-300 p-2'>{item?.description_en}</td>
+                <td className='border border-gray-300 p-2'>{item?.price}</td>
+                <td className='border border-gray-300 p-2'>{item?.category?.name_en}</td>
+                <td className='border border-gray-300 p-2'>{item?.colors?.[0]?.color_en}</td>
+                <td className='border border-gray-300 p-2'>{item?.sizes?.[0]?.size}</td>
+                <td className='border border-gray-300 p-2'>{item?.discount?.discount}</td>
+                <td className='border border-gray-300 p-2'>{Object.keys(item.materials)[0]}                </td>
+                <td className='border border-gray-300 p-2'>
+                  <button className='px-4 py-2 mr-2 bg-yellow-500 text-white rounded-lg hover:bg-yellow-600 transition cursor-pointer'>Edit</button>
+                  <button 
+                    onClick={()=>deleteProduccts(item.id)}
+                    className='px-4 py-2 bg-red-500 text-white rounded-lg hover:bg-red-600 transition cursor-pointer'>Delete</button>
+                </td>
+              </tr>
+            ))
+            }
           </tbody>
         </table>
       </div>
